3c54e24a66dbbd609430ab901e619af709d0aab7
maemo <maemo@ip-10-212-147-127.ec2.internal>
2010-05-10 00:23:38 +0000

    the big move

---
+++ flashlauncher/mainwindowline.cpp
@@ -0,0 +1,73 @@
+#include "mainwindowline.h"
+#include "ui_mainwindowline.h"
+
+#include <QtCore/QSettings>
+#include "simplefetch.h"
+#include "appdetail.h"
+#include "qdebug.h"
+
+
+#define SETTINGSPATH "/usr/share/flashlauncher/applications.conf"
+
+MainWindowLine::MainWindowLine(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::MainWindowLine)
+{
+    ui->setupUi(this);
+}
+
+MainWindowLine::~MainWindowLine()
+{
+    delete ui;
+}
+
+void MainWindowLine::loadLabels(QString groupname)
+{
+    appname = groupname;
+    QSettings settings(SETTINGSPATH, QSettings::IniFormat);
+    settings.beginGroup(groupname);
+    ui->desc->setText(settings.value("description","").toString());
+    ui->name->setText(settings.value("name", appname).toString());
+    ui->kbsize->setText(settings.value("size", appname).toString()+QString("KiB"));
+    ui->img->setGeometry(0, 0, 96, 96);
+    if (!settings.value("image").toString().isEmpty())
+    {
+        SimpleFetch * sf = new SimpleFetch(settings.value("image").toString());
+        connect(sf, SIGNAL(content(QByteArray)), this, SLOT(setImage(QByteArray)));
+        connect(sf, SIGNAL(content(QByteArray)), sf, SLOT(deleteLater())); // delete simplefetch after desc set
+    }
+}
+
+void MainWindowLine::setImage(QByteArray ba)
+{
+    QPixmap p;
+    p.loadFromData(ba);
+    if (p.size().isEmpty())
+        qDebug () << ba;
+    else
+        ui->img->setPixmap(QPixmap(p).scaled(96, 96));
+}
+
+void MainWindowLine::mousePressEvent(QMouseEvent * event)
+{
+    qDebug() << appname << " details";
+//    AppDetail* ad = new AppDetail(qobject_cast<QMainWindow*>(this->parent()));
+    AppDetail* ad = new AppDetail( qobject_cast<QMainWindow*>(qApp->activeWindow()));
+    ad->loadLabels(appname);
+#ifdef Q_WS_MAEMO_5
+    ad->setAttribute(Qt::WA_Maemo5StackedWindow);
+#endif
+    ad->show();
+}
+
+void MainWindowLine::changeEvent(QEvent *e)
+{
+    QWidget::changeEvent(e);
+    switch (e->type()) {
+    case QEvent::LanguageChange:
+        ui->retranslateUi(this);
+        break;
+    default:
+        break;
+    }
+}
---
+++ flashlauncher/mainwindowline.h
@@ -0,0 +1,31 @@
+#ifndef MAINWINDOWLINE_H
+#define MAINWINDOWLINE_H
+
+#include <QtGui/QWidget>
+
+namespace Ui {
+    class MainWindowLine;
+}
+
+class MainWindowLine : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindowLine(QWidget *parent = 0);
+    void loadLabels(QString groupname);
+    ~MainWindowLine();
+
+public slots:
+    void setImage(QByteArray ba);
+
+protected:
+    void changeEvent(QEvent *e);
+
+private:
+    void mousePressEvent(QMouseEvent * event);
+    QString appname;
+    Ui::MainWindowLine *ui;
+};
+
+#endif // MAINWINDOWLINE_H
